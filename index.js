dc3911616a5b8032d16d01d8d8125f823922b4f4
Juana Zorrilla <juanitazorrilla90@gmail.com>
2022-08-25 14:01:41 -0400

    Se creó la función calcularSalario

--- index.js
+++ index.js
@@ -2,7 +2,12 @@ let nombre = "Juana";
 let edad = calcularEdad(2003);
 console.log("Hola " + nombre);
 console.log(nombre + " tiene "+ edad + " años");
+console.log("Se le pagará: "+calcularSalario(16));
 
 function calcularEdad(anioNac){
     return new Date().getFullYear() - anioNac;
+}
+
+function calcularSalario(horasTrabajadas){
+    return horasTrabajadas * 20000
 }
